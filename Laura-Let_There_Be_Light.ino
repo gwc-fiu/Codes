909f96a18c0c20bef45796b63655ee81f4926cfd
gwc-fiu <40864822+gwc-fiu@users.noreply.github.com>
2018-07-05 14:54:17 -0400

    Create Laura-Let_There_Be_Light.ino

---
+++ Laura-Let_There_Be_Light.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ // h
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(360);
+//e
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(360);
+//y
+digitalWrite(11, HIGH);
+delay(360);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(120);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(360);
+digitalWrite(11, LOW);
+delay(120);
+digitalWrite(11, HIGH);
+delay(360);
+digitalWrite(11, LOW);
+delay(600);
+}
