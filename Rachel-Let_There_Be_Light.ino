5cb47382be4b5b5b6145415ffdc6e34eadf14585
gwc-fiu <40864822+gwc-fiu@users.noreply.github.com>
2018-07-05 13:19:53 -0400

    Create Rachel-Let_There_Be_Light.ino

---
+++ Rachel-Let_There_Be_Light.ino
@@ -0,0 +1,73 @@
+//Morse Code 
+void setup() {
+  pinMode(6, OUTPUT);
+  }
+
+void loop() {
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(360); //END LETTER
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(600);  // END WORD
+ digitalWrite(6, HIGH);
+ delay(360);
+ digitalWrite(6, LOW);
+ delay(360); // END LETTER
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(360);  // END LETTER
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(360);  // ENDS LETTER
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ digitalWrite(6, HIGH);
+ delay(360);
+ digitalWrite(6, LOW);
+ delay(360);  
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(360); // end letter
+ digitalWrite(6, HIGH);
+ delay(120);
+ digitalWrite(6, LOW);
+ delay(120);
+ delay(600); 
+
+}
