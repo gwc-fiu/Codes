c0713aad7b9ba12d18cc5f879937ca5edd1da856
gwc-fiu <40864822+gwc-fiu@users.noreply.github.com>
2018-07-05 13:16:38 -0400

    Create Nicole-Let_There_Be_Light.ino

---
+++ Nicole-Let_There_Be_Light.ino
@@ -0,0 +1,50 @@
+
+int ledPin= 7;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);   // H
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // A
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(2000);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(1000);  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(2000);                  // T
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(1000);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // S
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(100);                  // waits for a second
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(500);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(4000);                  // waits for a second
+}
